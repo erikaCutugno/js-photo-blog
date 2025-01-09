8374ae778f35ade5760569528b98926019c76c22
erikaCutugno <erikucciapop@gmail.com>
2025-01-09 14:59:25 +0100

    feat: add box-shadow

--- script.js
+++ script.js
@@ -7,12 +7,31 @@ axios.get("https://jsonplaceholder.typicode.com/photos?_limit=6")
 .then(function(response){
     const cardInfo = response.data
     cardInfo.forEach(card => {
-        console.log(card)
         cardAlbumElm.innerHTML += 
             `<div class="card">
-            <img class="img-point" src="./img/pin.svg" alt="puntina">
-             <img class="img-fluid" src="${card.url}" alt="image-1">
-            <p id="card-title">${card.title}</p>
+                <img class="img-point" src="./img/pin.svg" alt="puntina">
+                <img class="img-fluid" src="${card.url}" alt="image-1">
+                <p id="card-title">${card.title.toUpperCase()}</p>
             </div>`
     });
 })
+
+// axios.get("https://jsonplaceholder.typicode.com/photos?_limit=6")
+// .then(function(response){
+//     const cardInfo = response.data
+//     cardInfo.forEach(card => {
+//         const arrayTitle = card.title.split(" ")
+//         arrayTitle.forEach(elm => {
+//             const firstLetter = elm[0].toUpperCase()
+           
+//             let word = firstLetter
+//             for(let i=1; i< 4; i++){
+//                 word += elm[i]
+//             }
+//             console.log(word)
+        
+//         })
+       
+//         // console.log(arrayTitle)
+//     });
+// })

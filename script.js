8fd6ef7a2decbea952efb01d939b619a58ead7ff
erikaCutugno <erikucciapop@gmail.com>
2025-01-10 11:31:03 +0100

    feat: add overlay html e css

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 // DOM ELEMENTS
 const cardAlbumElm = document.getElementById("card-album")
-
+const overlayElm = document.getElementById("overlay")
+const btnElm = document.getElementById("btn")
+const imgBigElm = document.getElementById("img-big")
 //EVENTS
 
 axios.get("https://jsonplaceholder.typicode.com/photos?_limit=6")
@@ -10,7 +12,7 @@ axios.get("https://jsonplaceholder.typicode.com/photos?_limit=6")
         cardAlbumElm.innerHTML += 
             `<div class="card">
                 <img class="img-point" src="./img/pin.svg" alt="puntina">
-                <img class="img-fluid" src="${card.url}" alt="image-1">
+                <img class="img-fluid" src="${card.url}" alt="${card.title}">
                 <p id="card-title">${card.title}</p>
             </div>`
     });

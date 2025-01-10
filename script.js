16eca7df37db6d617b19d46f08d2e7023b0906f7
erikaCutugno <erikucciapop@gmail.com>
2025-01-10 11:53:19 +0100

    feat: add event click

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // DOM ELEMENTS
 const cardAlbumElm = document.getElementById("card-album")
-const overlayElm = document.getElementById("overlay")
+const bodyOverlayElm = document.getElementById("body-overlay")
 const btnElm = document.getElementById("btn")
 const imgBigElm = document.getElementById("img-big")
 //EVENTS
@@ -16,5 +16,18 @@ axios.get("https://jsonplaceholder.typicode.com/photos?_limit=6")
                 <p id="card-title">${card.title}</p>
             </div>`
     });
+
+    for(let i=0; i < 6; i++){
+        const cardElm = document.querySelectorAll(".card");
+
+        cardElm[i].addEventListener("click", function(){
+            bodyOverlayElm.classList.remove("d-none")
+            imgBigElm.src = `${cardInfo[i].url}`;
+            imgBigElm.alt = `${cardInfo[i].title}`;
+        })
+    }
 })
 
+btnElm.addEventListener("click",function(){
+    bodyOverlayElm.classList.add("d-none")
+})
